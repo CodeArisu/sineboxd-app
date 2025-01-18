31b192a568e0c30891d078b3da07712bba1d8616
CodeArisu <g.roland2604@gmail.com>
2025-01-18 08:51:53 +0800

    added example usage and default css config

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,26 @@
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/static/test.css') }}">
+@endpush
+
 @extends('layouts.app')
 
 <h1>If nakita ni tama ni</h1>
+
+<div>
+    <a href="">test</a>
+</div>
+
+<div>
+    <button>test</button>
+</div>
+
+<div>
+    <ul>
+        <li>list 1</li>
+        <li>list 2</li>
+        <li>list 3</li>
+    </ul>
+</div>
+
+
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <title>@yield('title', 'Sineboxd')</title>
-
+    
+    {{-- default css --}}
+    <link rel="stylesheet" href="{{ asset('css/var.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    {{-- 
+        applies css to a certain blade file without interruptions
+        with general style
+    --}}
+    @stack('styles')
 
 </head>
 
     <body>
+            {{-- 
+                Where html contents are being applied
+            --}}
         @yield('content')
     </body>
+
+    {{-- 
+        applies js to a certain blade file without interruptions
+    --}}
+    @stack('scripts')
     
 </html>
