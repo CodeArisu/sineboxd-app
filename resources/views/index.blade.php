58716634c0f45f5627df4201e1228bfa79754c17
CodeArisu <g.roland2604@gmail.com>
2025-02-04 08:56:50 +0800

    font/config

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,14 @@
 <h1>If nakita ni tama ni</h1>
 
 <div>
+    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae</h1>
+    <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae</h2>
+    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae</h3>
+    <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae</h4>
+    <h5>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae</h5>
+</div>
+
+<div>
     <a href="">test</a>
 </div>
 
